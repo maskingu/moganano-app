cef530ce1ec4617795753e8a98c4bcec88a8a45d
maskingu <kengomei@outlook.com>
2020-10-19 15:29:14 +0900

    likes_controllerの作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,22 @@
 class LikesController < ApplicationController
-  
+
+  before_action :set_valiables
+
+  def like
+    Like = current_user.likes.new(post.id: @post.id)
+    like.save
+  end
+
+  def unlike
+    like = current_user.likes.find_by(post.id: @post.id)
+    like.destroy
+  end
+
+  private
+
+  def set_valiables
+    @post = Post.find(params[:post_id])
+    @id_name = "#like-link-#{@post.id}"
+  end
+
 end
